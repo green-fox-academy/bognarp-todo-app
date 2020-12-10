9e3b335e13dd27453d5b55af48479ea60ba30715
Peter Bognar <21261487+bognarp@users.noreply.github.com>
2020-12-10 21:52:41 +0100

    implement basic print usage task

--- src/todo_app/Main.java
+++ src/todo_app/Main.java
@@ -2,7 +2,28 @@ package todo_app;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    handleArgs(args);
+  }
+
+  private static void handleArgs(String[] args) {
+    if (args.length == 0) {
+      // Print usage
+      System.out.println(usageInfo());
+    } else {
+      // TODO: Handle arguments
+      System.out.println(args);
     }
+  }
+
+  private static String usageInfo() {
+    return "Command Line Todo application\n" +
+        "=============================\n\n" +
+        "Command line arguments:\n" +
+        "\t-l\tLists all the tasks\n" +
+        "\t-a\tAdds a new task\n" +
+        "\t-r\tRemoves an task\n" +
+        "\t-c\tCompletes an task";
+  }
+
 }
